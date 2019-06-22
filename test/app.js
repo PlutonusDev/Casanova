c17b0e7a4f82d0281cbdcac021e15ed64576c6e6
= <plutonus.stream@gmail.com>
2019-06-23 02:16:56 +1000

    Added more details.

--- test/app.js
+++ test/app.js
@@ -1,7 +1,12 @@
 const Trio = require("../trio");
 const config = require("./data/config");
+const wait = require("util").promisify(setTimeout);
 
 const Client = new Trio.Client(config);
 const { token } = config
 
 Client.login(token);
+
+wait(5000).then(() => {
+    Client.destroy();
+});
